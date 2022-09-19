fdcd9883143beb452cfc817ab8af130e5af9c3e7
BadWolf45366 <112031099+BadWolf45366@users.noreply.github.com>
2022-09-18 19:31:09 -0600

    Add files via upload

---
+++ Conditionals.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(a == 2)
+        {
+            Console.WriteLine("Hello World")
+        }
+        if(b == 12)
+        {
+            Console.WriteLine("Okay!")
+        }
+        if(c == 9)
+        {
+            Console.WriteLine("It's dangerous to go alone, Take this")
+        }
+        if(d == 15)
+        {
+            Console.WriteLine("You wouldn't like me when I'm angry")
+        }
+        if(e == 13)
+        {
+            Console.WriteLine("Welcome to Jurassic Park")
+        }
+    }
+}
